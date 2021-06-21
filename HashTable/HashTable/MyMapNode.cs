34de711bd0a31fea7d208e60e0e13a2cb729a87c
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-21 19:21:17 +0530

    [Kumar Shubham]Added:ability to find freq of words in a sentence

---
+++ HashTable/HashTable/MyMapNode.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class MyMapNode<K,V> //Build a key Value pair HashTable   
+    {
+        ///Variable
+        public readonly int size; //size of Hash atble
+        ///Linked list of type key & value.
+        public readonly LinkedList<keyValue<K, V>>[] items;
+        /// <summary>
+        /// Initializes a Constructure with int Size Parameter
+        /// </summary>
+        /// <param name="size">The size.</param>
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<keyValue<K, V>>[size];
+        }
+        /// <summary>
+        /// Gets and sets the values.
+        /// </summary>
+        /// <typeparam name="k"></typeparam>
+        /// <typeparam name="v"></typeparam>
+        public struct keyValue<k, v> //key value is passed into the Above Linked list Key,value
+        {
+            public k key { get; set; }
+            public v value { get; set; }
+        }
+        /// <summary>
+        /// GetLinkedlist is a method and having a return type LinkedList key value pair.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <returns></returns>
+        protected LinkedList<keyValue<K, V>> GetLinkedlist(int position)
+        {
+            LinkedList<keyValue<K, V>> linkedLlist = items[position];
+            if (linkedLlist == null)
+            {
+                linkedLlist = new LinkedList<keyValue<K, V>>();
+                items[position] = linkedLlist;
+            }
+            return linkedLlist;
+        }
+        /// <summary>
+        /// Gets the array postion.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        protected int GetArrayPostion(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        /// <summary>
+        /// Gets the value provided by key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public V Get(K key) //Generic method define the value V
+        {
+            int position = GetArrayPostion(key); 
+            LinkedList<keyValue<K, V>> linkedlist = GetLinkedlist(position);
+            foreach (keyValue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    return item.value;
+                }
+            }
+            return default;
+        }
+        /// <summary>
+        /// Adds the specified key.
+        /// pushing the data into ther Hash table using the linked list operations.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPostion(key);
+            LinkedList<keyValue<K, V>> linkedlist = GetLinkedlist(position);
+
+            keyValue<K, V> item = new keyValue<K, V>() { key = key, value = value };
+            linkedlist.AddLast(item);
+        }
+        /// <summary>
+        /// Removes the value provided by key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public void Remove(K key)
+        {
+            int position = GetArrayPostion(key);
+            LinkedList<keyValue<K, V>> linkedlist = GetLinkedlist(position);
+
+            bool itemFound = false;
+
+            keyValue<K, V> foundItem = default(keyValue<K, V>);
+            foreach (keyValue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedlist.Remove(foundItem);
+            }
+        }
+    
+    }
+}
--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -7,6 +7,19 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("****Welcome to Hash Table****");
+            //UC1:-Ability to find Frequency of words in a sentence 
+            MyMapNode<string, string> node = new MyMapNode<string, string>(5);
+            node.Add("0", "To");
+            node.Add("1", "Be");
+            node.Add("2", "Or");
+            node.Add("3", "Not");
+            node.Add("4", "To");
+            node.Add("5", "Be");
+            string node0 = node.Get("0");
+            Console.WriteLine("1st index value: " + node0);
+            string node1 = node.Get("1");
+            Console.WriteLine("2nd index value: " +node1);
+            Console.WriteLine("3rd index value: "+node.Get("2"));
         }
     }
 }
